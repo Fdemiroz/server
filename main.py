4929436a0b8d7214d55907242abf7e4089d25450
Faruk Sofuoglu <faruk.sofuoglu@takeaway.com>
2021-07-23 16:05:05 +0200

    hallo

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from flask import Flask
-from flask_restful import Api, Resource
-
+from fastapi import FastAPI, Path
+from typing import Optional
 """
 ~ Author = Faroxx
 ~ Git =
+~ uvicorn main:app --reload
 """
-app = Flask(__name__)
-api = Api(app)
+app = FastAPI()
 
-if __name__ == "__main__":
-	app.run(debug=True)
+testruns = {
+	1: {
+		"name": "Run",
+		"status": "passed"
+	},
+	2: {
+		"name": "Valid",
+		"status": "failed"
+	}
+}
+
+@app.get("/runs")
+def runs():
+	return testruns
+
+@app.get("/runs/{item_id}")
+def get_run(item_id: int = Path(None, description="The ID of the test run", gt=0, lt=2)):
+	return testruns[item_id]
+
+@app.get("/run")
+def get_run(name: Optional[str] = None):
+	for id in testruns:
+		if testruns[id]["name"] == name:
+			return testruns[id]
+	return {"Data": "Not found"}
